84ac7eb19decf6a236e3f32ccc8e3af6387f630b
varspijoyan <varsikpijoyan2003@gmail.com>
2025-05-13 21:47:45 +0400

    Add main page component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
-import Header from './components/Header/Header';
+import "./App.css";
+import Header from "./components/Header/Header";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <>
       <Header />
+      <MainPage />
     </>
   );
 }
