999960ef4b1a58d32d1b819bbcc5f2b1cec865b4
varspijoyan <varsikpijoyan2003@gmail.com>
2025-05-17 20:06:54 +0400

    Format ShopPage.tsx

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import styles from "../styles/ShopPage.module.css";
-import { IProduct } from "../types";
 import { useNavigate } from "react-router-dom";
 import Product from "../components/ProductsSection/Product";
+import styles from "../styles/ShopPage.module.css";
+import { IProduct } from "../types";
 
 interface IShopPageProductsProps {
-    products: IProduct[];
+  products: IProduct[];
 }
 
 const ShopPage: React.FC<IShopPageProductsProps> = ({ products }) => {
-  const navigateTo = useNavigate();  
+  const navigateTo = useNavigate();
   return (
     <>
       <div className={styles.navigationLinks}>
-        <span onClick={() => navigateTo('/')}>
+        <span onClick={() => navigateTo("/")}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24px"
             viewBox="0 -960 960 960"
             width="24px"
             fill="black"
-            style={{cursor: "pointer"}}
+            style={{ cursor: "pointer" }}
           >
             <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
           </svg>
@@ -40,7 +40,7 @@ const ShopPage: React.FC<IShopPageProductsProps> = ({ products }) => {
       </div>
       <div className={styles.products}>
         {products.map((product) => (
-            <Product key={product.id} product={product} styles={styles}/>
+          <Product key={product.id} product={product} styles={styles} />
         ))}
       </div>
     </>
