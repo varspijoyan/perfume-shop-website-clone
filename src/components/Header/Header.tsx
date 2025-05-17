e237615a003aaf79f6f1a9f33e61d589e9663f54
varspijoyan <varsikpijoyan2003@gmail.com>
2025-05-17 20:09:19 +0400

    Add navigation to main path in header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/logo2x.png";
 import styles from "../../styles/Header.module.css";
 import Cart from "./Cart";
@@ -6,10 +7,16 @@ import HeaderNavLinks from "./HeaderNavLinks";
 import WishList from "./WishList";
 
 const Header: React.FC = () => {
+  const navigateTo = useNavigate();
   return (
     <header>
       <div className={styles.container}>
-        <img className={styles.logo} src={logo} alt="logo image" />
+        <img
+          className={styles.logo}
+          src={logo}
+          alt="logo image"
+          onClick={() => navigateTo("/")}
+        />
         <HeaderNavLinks />
         <div className={styles.wishListAndCartContent}>
           <WishList />
